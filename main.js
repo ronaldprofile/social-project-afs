b648bbea494f49703b68fa2e772d61e4463e0264
Ronald <ronaldtmprofile@gmail.com>
2021-08-20 12:16:31 -0300

    script created to animate the section (questions and answers)

--- main.js
+++ main.js
@@ -9,9 +9,31 @@ scrollReveal.reveal(
   `#home .text, #home .image,
   #services .card,
   #i-will-learn .card,
+  #questions .question,
   #social-networks .youtube,
   #social-networks .instagram,
   #social-networks .discord,
   footer .brand, footer .list`,
   { interval: 100 }
 );
+
+/** Questions and answers */
+
+const buttonShowAnswer = document.querySelectorAll(".open-answer");
+
+buttonShowAnswer.forEach(button => {
+  button.addEventListener("click", event => {
+    const questionId = event.target.dataset.question;
+
+    const questionIcon = document.querySelector(
+      `button .icon[data-question="${questionId}"]`
+    );
+
+    const questionItem = document.querySelector(
+      `.response[data-question="${questionId}"]`
+    );
+
+    questionItem.classList.toggle("active");
+    questionIcon.classList.toggle("animate-icon");
+  });
+});
